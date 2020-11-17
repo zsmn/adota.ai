6a0bf99c256df725b28f4046172489f1b4d80006
zsmn <zsmn@cin.ufpe.br>
2020-11-17 15:10:37 -0300

    Cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 // server.js
-console.log('May Node be with you')
-
 const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
@@ -10,10 +8,6 @@ var pets = null;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
-console.log(process.env.MONGO_USR)
 
 MongoClient.connect(process.env.MONGO_USR, { useUnifiedTopology: true })
   .then(client => {
@@ -24,51 +18,21 @@ MongoClient.connect(process.env.MONGO_USR, { useUnifiedTopology: true })
 .catch(error => console.error(error))
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('listening')
+    console.log('Listening!')
 })
 
 app.get('/', (req, res) => {
-    db.collection('pets').find().toArray()
-    .then(results => {
-      res.render('index.ejs', { pets: results })
-    })
-    .catch(error => console.error(error))
+
 })
 
 app.post('/pets', (req, res) => {
-    pets.insertOne(req.body)
-    .then(result => {
-        res.redirect('/')
-    })
-    .catch(error => console.error(error))
+
 })
   
 app.put('/pets', (req, res) => {
-    pets.findOneAndUpdate(
-        { name: 'Zildao' },
-        {
-            $set: {
-              name: req.body.name,
-              desc: req.body.desc
-            }
-        },
-        {
-            upsert: true
-        }
-    )
-    .then(result => console.log("Updated Zildao successfuly"))
-    .catch(error => console.error(error))
+
 })
 
 app.delete('/pets', (req, res) => {
-    pets.deleteOne(
-        { name: 'Zildao' }
-    )
-    .then(result => {
-        if (result.deletedCount === 0) {
-            return res.json('No zildao to delete')
-        }
-        res.json(`Zildao deleted`)
-    })
-    .catch(error => console.error(error))
+
 })
