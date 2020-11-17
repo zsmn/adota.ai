163b14ac87adefc779599c36af3a824f093b99f1
zsmn <zsmn@cin.ufpe.br>
2020-11-17 15:43:13 -0300

    Now working for events and pets

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,12 @@ app.listen(process.env.PORT || 3000, function() {
 })
 
 app.get('/', (req, res) => {
+    
+})
+
+/// pets
+// get
+app.get('/pets', (req, res) => {
     db.collection('pets').find().toArray()
     .then(results => {
       res.json(results)
@@ -29,18 +35,42 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.get('/pets', (req, res) => {
-
-})
-
+// post
 app.post('/pets', (req, res) => {
 
 })
-  
+
+// put
 app.put('/pets', (req, res) => {
 
 })
 
+// delete
 app.delete('/pets', (req, res) => {
+
+})
+
+/// events
+// get
+app.get('/events', (req, res) => {
+    db.collection('events').find().toArray()
+    .then(results => {
+      res.json(results)
+    })
+    .catch(error => console.error(error))
+})
+
+// post
+app.post('/events', (req, res) => {
+
+})
+
+// put
+app.put('/events', (req, res) => {
+
+})
+
+// delete
+app.delete('/events', (req, res) => {
 
 })
