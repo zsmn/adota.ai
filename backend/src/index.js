e055ee5f0d50e7488b924ae1934b40a702c8df39
zsmn <zsmn@cin.ufpe.br>
2020-11-19 13:35:07 -0300

    headers

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+    res.setHeader("Access-Control-Max-Age", "3600");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+	next();
+});
+
 require('./controllers/authController')(app);
 require('./controllers/petController')(app);
 require('./controllers/eventController')(app);
