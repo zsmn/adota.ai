7b2103b8d1a927412ab0a9b73c6b341fa8584b67
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-11-21 02:13:39 -0300

    Update loginController.js

--- site/loginController.js
+++ site/loginController.js
@@ -1,24 +1,24 @@
-var showUserAndLogoutButton = true; //So mostro isso quando o usuario tiver conectado
-var userName = "vmmc2";
+var userName = "";
+
+async function checkToken(document){
+    var cabecalho = document.getElementById("cabecalho")
+    await axios.post('https://adota-ai-backend.herokuapp.com/auth/requestuser', {
+        token : localStorage.getItem('token')
+    })
+      .then(resp => {
+        userName = resp.data.username;
+        cabecalho.innerHTML += "<li><a id=\"logoutButton\" href=\"index.html\">Logout</a></li> <li><a href=\"#\">"+userName+"</li>"
+        document.getElementById('logoutButton').onclick = function() {
+            localStorage.setItem('token', '')
+            window.location.href = "https://adota-ai.herokuapp.com/index.html";
+        }
+      })
+      .catch(error => {
+        cabecalho.innerHTML += "<li><a href=\"login.html\">Login</a></li><li><a href=\"cadastro.html\">Cadastro</a></li>"
+      });
+    console.log(localStorage.getItem('token'));
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    var cabecalho = document.getElementById("cabecalho")
-    if(showUserAndLogoutButton == true){ 
-        //Tiro os botoes de 'cadastro' e de 'login'
-        //Mostro o botao de 'logout' e o userName
-        cabecalho.innerHTML += "<li><a href=\"index.html\">Logout</a></li> <li><a href=\"#\">\"vmmc2\"</li>"
-    }else{
-        cabecalho.innerHTML += "<li><a href=\"login.html\">Login</a></li><li><a href=\"cadastro.html\">Cadastro</a></li>"
-    }
+    checkToken(document);
 })
-/*
-<li><a href="login.html">Login</a></li>
-<li><a href="cadastro.html">Cadastro</a></li>
-
-
-<li class="active"><a href="#">Login</a></li>
-<li><a href="cadastro.html">Cadastro</a></li>
-
-<li><a href="login.html">Login</a></li>
-<li class="active"><a href="#">Cadastro</a></li>
-*/
