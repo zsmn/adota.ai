4edc86a107b67fd47276bff85ac125221857f6b3
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-11-24 18:02:03 -0300

    Update AddContentDinamically2.js

--- site/AddContentDinamically2.js
+++ site/AddContentDinamically2.js
@@ -78,27 +78,67 @@ async function getAnimalsInfo(document){
             }
 
             if(i % 12 == 0){ //Devo adicionar um novo slide.
-                carouselSlides.innerHTML += "<div class=\"carousel-item\"><table class=\"table-card-bichinhos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"row-bichinhos\"><td> <div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary\" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div>  </td></tr></table></div>"
+                carouselSlides.innerHTML += "<div class=\"carousel-item\"><table class=\"table-card-bichinhos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"row-bichinhos\"><td> <div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div>  </td></tr></table></div>"
             }
             else{ //Adiciono apenas um novo card dentro do slide.
                 allTableCard = Array.from(document.querySelectorAll('.table-card-bichinhos'))
                 if(i % 4 == 0){ //Devo inserir uma row dentro da table presente no slide.
                     var lastTableCard = allTableCard[allTableCard.length - 1]
-                    lastTableCard.innerHTML += "<tr class=\"row-bichinhos\"><td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary\" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td></tr>"
+                    lastTableCard.innerHTML += "<tr class=\"row-bichinhos\"><td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td></tr>"
                 }
                 else{ //Posso inserir o card direto dentro da row
                     var allRows = Array.from(document.querySelectorAll('.row-bichinhos'))
                     var lastRow = allRows[allRows.length - 1]
-                    lastRow.innerHTML += "<td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary\" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td>"
+                    lastRow.innerHTML += "<td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td>"
                 }
             }
         }
+        var arrayBotoesVejaMais = Array.from(document.querySelectorAll('.botao-veja-mais'));
+        var petNames = [] 
+        var petLocalities = [] 
+        var petSize = [] 
+        var petSex = [] 
+        var petAge = [] 
+        var petContact = [] 
+        var petInfo = [] 
+        var petPhoto1 = []
+        var petPhoto2 = []
+        var petPhoto3 = []
+        for(var i = 0; i < arrayBotoesVejaMais.length; i++){
+            var id = parseInt(arrayBotoesVejaMais[i].id);
+            var animalName = finalBd[id]["animalName"] 
+            var animalLocality = finalBd[id]["locality"]
+            var animalSize = finalBd[id]["size"]
+            var animalSex = finalBd[id]["sex"]
+            var animalAge = finalBd[id]["age"]
+            var animalContact = finalBd[id]["contact"]
+            var animalInfo = finalBd[id]["info"]
+            var animalPhoto1 = finalBd[id]["photos"][0]
+            var animalPhoto2 = finalBd[id]["photos"][1]
+            var animalPhoto3 = finalBd[id]["photos"][2]
+            petNames.push(animalName)
+            petLocalities.push(animalLocality)
+            petSize.push(animalSize)
+            petSex.push(animalSex)
+            petAge.push(animalAge)
+            petContact.push(animalContact)
+            petInfo.push(animalInfo)
+            petPhoto1.push(animalPhoto1)
+            petPhoto2.push(animalPhoto2)
+            petPhoto3.push(animalPhoto3)
+            arrayBotoesVejaMais[i].onclick = function () {
+                Swal.fire({
+                    title: "<h2>" + petNames[this.id] + "</h2>",
+                    html: "<div><img src="+petPhoto1[this.id]+" class=\"card-img-titulo-top\" alt=\"Imagem 1 não adicionada\"></div> <p><h5> <strong>Localidade</strong>:  " + petLocalities[this.id] + "</h5></p> <p><h5><strong>Porte</strong>:  " + petSize[this.id] + "</h5></p> <p><h5><strong>Sexo</strong>:  " + petSex[this.id] + "</h5></p> <p><h5><strong>Idade</strong>:  " + petAge[this.id] + "</h5></p> <p><h5><strong>Informações</strong>:  " + petInfo[this.id] + "</h5></p> <p><h5><strong>Contato</strong>:  " + petContact[this.id] + "</h5></p> <div><img src="+petPhoto2[this.id]+" class=\"card-img-top\" alt=\"Imagem 2 não adicionada\"> <img src="+petPhoto3[this.id]+" class=\"card-img-top\" alt=\"Imagem 3 não adicionada\"></div>",
+                    background: "#75E3DE",
+                });
+            }
+        }
+        
+        console.log(document.querySelectorAll('.botao-veja-mais'))
     });
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {    
     getAnimalsInfo(document)
 })
-
-
