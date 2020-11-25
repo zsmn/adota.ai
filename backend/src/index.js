05296cc9b4613999ac9f56932bd5b34eaee4f1de
zsmn <zsmn@cin.ufpe.br>
2020-11-24 22:11:48 -0300

    Using cors()

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-    res.setHeader("Access-Control-Max-Age", "3600");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-	next();
-});
+app.use(cors());
 
 require('./controllers/authController')(app);
 require('./controllers/petController')(app);
