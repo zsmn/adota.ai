77a2a1954539eae13398089e72d88024a900fd5b
zsmn <zsmn@cin.ufpe.br>
2020-11-24 22:26:58 -0300

    higher limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
+
 
 require('./controllers/authController')(app);
 require('./controllers/petController')(app);
