371b20fa9c53af52b2c0aa81ae444a8cb0f34ed2
zsmn <zsmn@cin.ufpe.br>
2020-11-24 23:27:58 -0300

    Now deleting correctly

--- site/AddContentDinamically2.js
+++ site/AddContentDinamically2.js
@@ -2,33 +2,23 @@
 
 var contentBd
 var finalBd
+var auxiliar = ""
+
+function redirect(){
+    window.location.href = "https://adota-ai.herokuapp.com/bichinhos.html";
+}
 
 async function getAnimalsInfo(document){
     await axios.get('https://adota-ai-backend.herokuapp.com/pet')
       .then(resp => {
-        
-        //console.log(resp.data);
         contentBd = resp.data;
         finalBd = contentBd['pets'] //Aqui, eu consigo acessar cada um dos pets cadastrados em nosso Bd.
-        /*
-        console.log('olaaaaa');
-        console.log(contentBd['pets']);
-        console.log(contentBd['pets'][0]);
-        console.log(contentBd['pets'][1]);
-        console.log(contentBd['pets'][0]['age']);
-        console.log(contentBd['pets'][0]['animalName']);
-        */
+       
         //Fazendo a conversão de um Array para u JSON
-        
-        
-        
         var carouselSlides = document.getElementById("carousel-inner")
-
         var quantityOfCardAnimals = 0
         var allCardAnimals = Array.from(document.querySelectorAll('.card'))
         var allCarouselItem = Array.from(document.querySelectorAll('.carousel-item'))
-        console.log(allCardAnimals.length)
-        console.log(allCarouselItem.length)
 
         quantityOfCardAnimals = allCardAnimals.length
 
@@ -45,16 +35,15 @@ async function getAnimalsInfo(document){
         var INFO_ANIMAL = ""
         var FOTO_ANIMAL_2 = ""
         var FOTO_ANIMAL_3 = ""
+        var ID_ANIMAL = ""
         
         //Verificar se teremos que usar o metodo Array.from() de novo.
         //Verificar se teremos que usar o metodo Array.from() de novo.
 
         var allTableCard = Array.from(document.querySelectorAll('.table-card-bichinhos'))
 
-        //console.log(contentBd)
-        //console.log(contentBd.length)
-
-        for(var i = 0; i < finalBd.length; i++){
+        var j = 10000;
+        for(var i = 0; i < finalBd.length; i++, j++){
             //console.log("DALEEE")
             FOTO_ANIMAL = finalBd[i]["photos"][0]
             NOME_ANIMAL = finalBd[i]["animalName"]
@@ -78,18 +67,18 @@ async function getAnimalsInfo(document){
             }
 
             if(i % 12 == 0){ //Devo adicionar um novo slide.
-                carouselSlides.innerHTML += "<div class=\"carousel-item\"><table class=\"table-card-bichinhos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"row-bichinhos\"><td> <div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div>  </td></tr></table></div>"
+                carouselSlides.innerHTML += "<div class=\"carousel-item\"><table class=\"table-card-bichinhos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"row-bichinhos\"><td> <div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><div><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a> <a href=\"#\" class=\"btn btn-primary botao-deletar\" id="+j.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a>  </div></center></div></div> </td></tr></table></div>"
             }
             else{ //Adiciono apenas um novo card dentro do slide.
                 allTableCard = Array.from(document.querySelectorAll('.table-card-bichinhos'))
                 if(i % 4 == 0){ //Devo inserir uma row dentro da table presente no slide.
                     var lastTableCard = allTableCard[allTableCard.length - 1]
-                    lastTableCard.innerHTML += "<tr class=\"row-bichinhos\"><td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td></tr>"
+                    lastTableCard.innerHTML += "<tr class=\"row-bichinhos\"><td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><div><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a> <a href=\"#\" class=\"btn btn-primary botao-deletar\" id="+j.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a></div></center></div></div> </td></tr>"
                 }
                 else{ //Posso inserir o card direto dentro da row
                     var allRows = Array.from(document.querySelectorAll('.row-bichinhos'))
                     var lastRow = allRows[allRows.length - 1]
-                    lastRow.innerHTML += "<td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a></center></div></div> </td>"
+                    lastRow.innerHTML += "<td><div class=\"card\" style=\"width: 20rem; background-color: #75E3DE;\"><div class=\"card-body\"><table class=\"content-of-card\"><tr><td> <img class=\"card-img-top\" src="+FOTO_ANIMAL+" alt=\"Card image cap\"> </td><td><h5 class=\"card-title\">"+NOME_ANIMAL+"</h5><table><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/location_icon.png\"> </td><td> <p class=\"card-text\">"+LOCALIDADE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/size_icon.png\"> </td><td> <p class=\"card-text\">"+PORTE_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src="+ICONE_SEXO_ANIMAL+"> </td><td> <p class=\"card-text\"> "+SEXO_ANIMAL+"<p> </td></tr><tr><td> <img class=\"icon-image\" src=\"assets/img/icons/age_icon.png\"> </td><td> <p class=\"card-text\"> "+IDADE_ANIMAL+"<p> </td></tr></table></td></tr></table><br><center><div><a href=\"#\" class=\"btn btn-primary botao-veja-mais\" id="+i.toString()+" style=\"background-color: #FAFF00; height: 40px ;color: black;\"><table><tr><td><img class=\"icon-image-button\" src=\"assets/img/icons/moreinfo_icon.png\"><td> <p class=\"button-text\">Veja mais</p></td></td></tr></table></a> <a href=\"#\" class=\"btn btn-primary botao-deletar\" id="+j.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a> </div></center></div></div> </td>"
                 }
             }
         }
@@ -104,6 +93,7 @@ async function getAnimalsInfo(document){
         var petPhoto1 = []
         var petPhoto2 = []
         var petPhoto3 = []
+        var petIDs = []
         for(var i = 0; i < arrayBotoesVejaMais.length; i++){
             var id = parseInt(arrayBotoesVejaMais[i].id);
             var animalName = finalBd[id]["animalName"] 
@@ -127,6 +117,7 @@ async function getAnimalsInfo(document){
             petPhoto2.push(animalPhoto2)
             petPhoto3.push(animalPhoto3)
             arrayBotoesVejaMais[i].onclick = function () {
+                //console.log("this.id veja mais = " + this.id);
                 Swal.fire({
                     title: "<h2>" + petNames[this.id] + "</h2>",
                     html: "<div><img src="+petPhoto1[this.id]+" class=\"card-img-titulo-top\" alt=\"Imagem 1 não adicionada\"></div> <p><h5> <strong>Localidade</strong>:  " + petLocalities[this.id] + "</h5></p> <p><h5><strong>Porte</strong>:  " + petSize[this.id] + "</h5></p> <p><h5><strong>Sexo</strong>:  " + petSex[this.id] + "</h5></p> <p><h5><strong>Idade</strong>:  " + petAge[this.id] + "</h5></p> <p><h5><strong>Informações</strong>:  " + petInfo[this.id] + "</h5></p> <p><h5><strong>Contato</strong>:  " + petContact[this.id] + "</h5></p> <div><img src="+petPhoto2[this.id]+" class=\"card-img-top\" alt=\"Imagem 2 não adicionada\"> <img src="+petPhoto3[this.id]+" class=\"card-img-top\" alt=\"Imagem 3 não adicionada\"></div>",
@@ -134,8 +125,29 @@ async function getAnimalsInfo(document){
                 });
             }
         }
-        
-        console.log(document.querySelectorAll('.botao-veja-mais'))
+
+        var arrayBotoesDelecao = Array.from(document.querySelectorAll('.botao-deletar'));
+        for(var i = 0; i < arrayBotoesDelecao.length; i++){
+            var animalID = finalBd[i]['_id']
+            petIDs.push(animalID)
+
+            arrayBotoesDelecao[i].onclick = async function () {
+                var id = this.id - 10000;
+                console.log(id);
+                await axios.post('https://adota-ai-backend.herokuapp.com/pet/delete/'+ petIDs[id], { },{ headers: {'Authorization' : localStorage.getItem('token')}})
+                .catch(err => {
+                    alertify.error('Você não tem autorização para isso')
+                })
+                .then(res => {
+                    if(res){
+                        if(res.statusText == "OK"){
+                            alertify.success('Animal deletado com sucesso, redirecionando...');
+                            setTimeout(redirect, 2000)
+                        }
+                    }
+                })
+            }
+        }
     });
 }
 
