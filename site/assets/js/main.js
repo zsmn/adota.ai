b8a8e58ffa5614d65efabbaaf9f36895d37df032
VictorHMS <39633965+VictorHMS@users.noreply.github.com>
2020-11-25 21:19:42 -0300

    Update main.js
    Fazer o Carrossel parar de andar sozinho

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -141,7 +141,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     items: 1
@@ -167,7 +167,7 @@
 
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     items: 1
