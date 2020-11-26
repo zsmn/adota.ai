681d058db7484a051b8f14c62dc33b264b9c44f7
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-11-25 22:21:00 -0300

    Update AddContentDinamically.js

--- site/AddContentDinamically.js
+++ site/AddContentDinamically.js
@@ -2,14 +2,27 @@
 
 var contentBd
 var finalBd
+var userid = "-1"
+
+function redirect(){
+    window.location.href = "https://adota-ai.herokuapp.com/eventos.html";
+}
 
 async function getEventsInfo(document){
     await axios.get('https://adota-ai-backend.herokuapp.com/event')
-      .then(resp => {
-        //console.log(resp.data);
+      .then(async resp => {
+        if(localStorage.getItem('token')){
+            await axios.post('https://adota-ai-backend.herokuapp.com/auth/requestuser', {
+                token : localStorage.getItem('token')
+            })
+            .then(resp => {
+                userid = resp.data._id;    
+            })
+        }
+
         contentBd = resp.data;
         finalBd = contentBd['events'] //Assim eu consigo acessar cada um dos eventos que se encontram presentes dentro do meu bd.
-        //console.log(contentBd['events'])
+        console.log(finalBd);
 
         var carouselSlides = document.getElementById("carousel-inner")
 
@@ -29,6 +42,7 @@ async function getEventsInfo(document){
         var PHOTO3_DO_EVENTO = ""
         var PHOTO4_DO_EVENTO = ""
         var CONTATO_DO_EVENTO = ""
+        var ID_DONO_EVENTO = ""
         var d = ""
         var date = []
         var date1 = ""
@@ -49,7 +63,7 @@ async function getEventsInfo(document){
             //aux[2] -> day
             year = aux[0].substr(2,2)
             DATA_DO_EVENTO = aux[2] + "/" + aux[1] + "/" + year
-            console.log("data do evento: " + DATA_DO_EVENTO)
+            //console.log("data do evento: " + DATA_DO_EVENTO)
             /************************************************/
             DESCRICAO_DO_EVENTO = finalBd[i]['generalInfo']
             DESCRICAO_DO_EVENTO_2 = finalBd[i]['extraInfo']
@@ -57,20 +71,53 @@ async function getEventsInfo(document){
             PHOTO3_DO_EVENTO = finalBd[i]['photos'][2]
             PHOTO4_DO_EVENTO = finalBd[i]['photos'][3]
             CONTATO_DO_EVENTO = finalBd[i]['ownerContact']
+            ID_DONO_EVENTO = finalBd[i]['userId']
 
             if(i % 3 == 0){ //Tem que adicionar uma nova pagina 
-                carouselSlides.innerHTML += "<div class=\"carousel-item \"> <table class=\"table-card-eventos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"identifier-row\">  <td><div class=\"card-eventos\" style=\"width: 18rem;\"><div class=\"card-body-eventos\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p> </center></div></div></div></td>  </tr></table></div>"
+                if(ID_DONO_EVENTO == userid){ //Mostro o botao de deletar
+                    carouselSlides.innerHTML += "<div class=\"carousel-item \"> <table class=\"table-card-eventos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"identifier-row\">  <td><div class=\"card-eventos\" style=\"width: 18rem; height: 33rem;\"><div class=\"card-body-eventos\" style=\"height: 33rem;\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p></center></div><div> <center><a href=\"#\" class=\"btn btn-primary botao-deletar\" id="+i.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a></center></div></div></div></td>  </tr></table></div>"
+                }else{ //Nao mostro o botao de deletar
+                    carouselSlides.innerHTML += "<div class=\"carousel-item \"> <table class=\"table-card-eventos\" align=\"center\" style=\"padding: 20px;\"><tr class=\"identifier-row\">  <td><div class=\"card-eventos\" style=\"width: 18rem;\"><div class=\"card-body-eventos\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p> </center></div> <div> <center><a hidden href=\"#\" class=\"btn btn-primary botao-deletar\" id="+i.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a></center> </div>  </div></div></td>  </tr></table></div>"
+                }
                 //allCarouselItem = Array.from(document.querySelectorAll('.carousel-item'))
                 //var lastCarouselItem = allCarouselItem[allCarouselItem.length - 1]
                 //lastCarouselItem.innerHTML += "<div style=\"background-color: red; width: 50px; height: 50px;\"> </div>"
             }
             else{ //Tem um ou dois espacos vagos na ultima pagina.
-                allCarouselTr = Array.from(document.querySelectorAll('.identifier-row'))
-                var lastTr = allCarouselTr[allCarouselTr.length - 1]
-                lastTr.innerHTML += "<td><div class=\"card-eventos\" style=\"width: 18rem;\"><div class=\"card-body-eventos\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p> </center></div></div></div></td>"
-    
+                if(ID_DONO_EVENTO == userid){ //Mostro o botao de deletar
+                    allCarouselTr = Array.from(document.querySelectorAll('.identifier-row'))
+                    var lastTr = allCarouselTr[allCarouselTr.length - 1]
+                    lastTr.innerHTML += "<td><div class=\"card-eventos\" style=\"width: 18rem; height: 33rem;\"><div class=\"card-body-eventos\" style=\"height: 33rem;\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p> </center></div> <div> <center><a href=\"#\" class=\"btn btn-primary botao-deletar\" id="+i.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a></center></div></div></div></td>"
+                }else{ //Nao mostro o botao de deletar
+                    allCarouselTr = Array.from(document.querySelectorAll('.identifier-row'))
+                    var lastTr = allCarouselTr[allCarouselTr.length - 1]
+                    lastTr.innerHTML += "<td><div class=\"card-eventos\" style=\"width: 18rem;\"><div class=\"card-body-eventos\"><table class=\"content-of-event-card\"><tr><img class=\"card-eventos-img-top\" src="+IMAGEM_PRINCIPAL_EVENTO+" alt=\"Card image cap\"></tr><tr><td><div class=\"event-date-box\"><p class=\"event-date\"> "+DATA_DO_EVENTO+" </p></div></td><td style=\"margin-right: 5px;\"><div class=\"event-descript1-box\"><p class=\"event-descript1\"> "+DESCRICAO_DO_EVENTO+" </p></div></td></tr></table><div class=\"event-descript2\"><p> "+DESCRICAO_DO_EVENTO_2+" </p></div><table class=\"event-photos\"><tr><td><img class=\"event-photo\" src="+PHOTO2_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO3_DO_EVENTO+" alt=\"Here goes an image!\"></td><td><img class=\"event-photo\" src="+PHOTO4_DO_EVENTO+" alt=\"Here goes an image!\"></td></tr></table><div class=\"event-contact\"><center> <p> "+CONTATO_DO_EVENTO+" </p> </center></div> <div><center><a hidden href=\"#\" class=\"btn btn-primary botao-deletar\" id="+i.toString()+" style=\"background-color: red; height: 40px; color: black;\"> Deletar </a></center></div> </div></div></td>"
+                }
                 //var lastCarouselItem = allCarouselItem[allCarouselItem.length - 1]
                 //lastCarouselItem.append("<div style=\"background-color: red; width: 50px; height: 50px;\"> </div>")    
+            }
+        }
+        var eventIDs = [];
+        var arrayBotoesDelecao = Array.from(document.querySelectorAll('.botao-deletar'));
+        for(var i = 0; i < arrayBotoesDelecao.length; i++){
+            var eventID = finalBd[i]["_id"];
+            console.log("eventID = " + eventID);
+            eventIDs.push(eventID);
+
+            arrayBotoesDelecao[i].onclick = async function () {
+                console.log("dentro do function = " + eventIDs[this.id]);
+                await axios.post('https://adota-ai-backend.herokuapp.com/event/delete/'+ eventIDs[this.id], { },{ headers: {'Authorization' : localStorage.getItem('token')}})
+                .catch(err => {
+                    alertify.error('Você não tem autorização para isso')
+                })
+                .then(res => {
+                    if(res){
+                        if(res.statusText == "OK"){
+                            alertify.success('Evento deletado com sucesso, redirecionando...');
+                            setTimeout(redirect, 2000)
+                        }
+                    }
+                })
             }
         }
           
